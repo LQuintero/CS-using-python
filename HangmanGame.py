d473fb9c739afeda6286d626c856850da19896e3
Laura Quintero <quintero.laura.a@gmail.com>
2015-10-14 09:37:43 -0500

    Update HangmanGame.py

--- HangmanGame.py
+++ HangmanGame.py
@@ -3,10 +3,7 @@
 #
 
 # -----------------------------------
-# Helper code
-# You don't need to understand this helper code,
-# but you will have to know how to use the functions
-# (so be sure to read the docstrings!)
+
 
 import random
 import string
